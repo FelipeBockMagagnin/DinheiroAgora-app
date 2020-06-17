b4b33c24984f8ed05835c0f72e4c9bdd87b5a670
Felpi <felipebmagagnin@gmail.com>
2020-06-17 17:51:04 -0300

    create base files, add axios

---
+++ src/services/awesomeApiCoins.js
@@ -0,0 +1,16 @@
+//Make requisitions to this public api
+//https://docs.awesomeapi.com.br/api-de-moedas
+
+import axios from 'axios'
+
+const baseUrl = 'https://economia.awesomeapi.com.br/json'
+
+function GetAllCoins(){
+  return axios.get(baseUrl + "/all")
+}
+
+function GetCoinHistory(coinID, quantity){
+  return axios.get(baseUrl + `/${coinID}/${quantity}`)
+}
+
+export { GetAllCoins, GetCoinHistory }

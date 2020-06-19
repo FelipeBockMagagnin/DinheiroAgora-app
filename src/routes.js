77ffc649e59ceafbd985575a234c5689207a548d
Felpi <felipebmagagnin@gmail.com>
2020-06-19 08:46:38 -0300

    create routes and convert page

---
+++ src/routes.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './pages/home';
+import { Feather } from '@expo/vector-icons';
+import Convert from './pages/convert';
+
+
+const Tab = createBottomTabNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Listar') {
+              iconName = 'dollar-sign';
+            } else if (route.name === 'Converter') {
+              iconName = 'shuffle';
+            }
+
+            // You can return any component that you like here!
+            return <Feather name={iconName} size={size} color={color} />
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Listar" component={Home} />
+        <Tab.Screen name="Converter" component={Convert} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}

005b481dc6b74c1c07d69708608f9d1423b66047
Felpi <felipebmagagnin@gmail.com>
2020-06-25 13:48:47 -0300

    Changes on coversion page

--- src/pages/convert/index.js
+++ src/pages/convert/index.js
@@ -6,22 +6,24 @@ import { GetAllCoins } from '../../services/awesomeApiCoins'
 
 export default function Convert() {
   const [coins, setCoins] = useState()
+
   const [selectedValue, setSelectedValue] = useState('USD');
   const [selectedTargetValue, setSelectedTargetValue] = useState('USD');
-  const [inputValue, setInputvalue] = useState('1,00')
-  const [inputTargetValue, setInputTargetValue] = useState('1,00')
+
+  const [inputValue, setInputValue] = useState('1.00')
+  const [inputTargetValue, setInputTargetValue] = useState('1.00')
 
   useEffect(() => {
     GetAllCoins().then(data => {
       setCoins(Object.values(data.data))
-    })
+    });
   }, [])
 
   if (coins === undefined) {
     return <Loading></Loading>
   }
   
-  function calculateConversion() {
+  function calculateFromValue() {
     if(inputTargetValue === undefined || inputValue === undefined){
       alert('Por favor, insira valores nos campos')
       return
@@ -30,12 +32,19 @@ export default function Convert() {
     const fromCoin = coins.find(element => element.code == selectedValue).bid;
     const toCoin = coins.find(element => element.code == selectedTargetValue).bid;
 
-    const conversionRateTo = fromCoin / toCoin;
-    const conversionRateFrom = toCoin / fromCoin;
+    const conversionRateTo = fromCoin / toCoin;    
+    const fromValue = parseFloat(conversionRateTo) * parseFloat(inputTargetValue);
 
-    //alert(conversionRateFrom +  conversionRateTo)
-    setInputvalue(conversionRateTo * Number(inputTargetValue));
-    setInputTargetValue(conversionRateFrom * Number(inputValue));
+    setInputValue(fromValue.toFixed(4).toString());
+  }
+  
+  function calculateToValue(){
+    const fromCoin = coins.find(element => element.code == selectedValue).bid;
+    const toCoin = coins.find(element => element.code == selectedTargetValue).bid;
+
+    const conversionRateFrom = toCoin / fromCoin;
+    const toValue = parseFloat(conversionRateFrom) * parseFloat(inputValue);
+    setInputTargetValue(toValue.toFixed(4).toString());
   }
 
   return (
@@ -45,13 +54,14 @@ export default function Convert() {
       <View style={styles.innerContainer}>
         <TextInput
           value={inputValue}
-          onChangeText={(value) => setInputvalue(value)}
+          onChangeText={(value) => { setInputValue(value); calculateToValue() }}
           style={styles.textInput}
         />
+
         <View style={styles.pickerInput}>
           <Picker
             selectedValue={selectedValue}
-            onValueChange={(itemValue, itemIndex) => { setSelectedValue(itemValue); calculateConversion() }}>
+            onValueChange={(itemValue, itemIndex) => { setSelectedValue(itemValue); calculateToValue() }}>
             {coins.map(coin => <Picker.Item label={coin.name} value={coin.code} />)}
           </Picker>
         </View>
@@ -60,14 +70,14 @@ export default function Convert() {
       <View style={styles.innerContainer}>
         <TextInput
           value={inputTargetValue}
-          onChangeText={(value) => setInputTargetValue(value)}
+          onChangeText={(value) => { setInputTargetValue(value); calculateFromValue() }}
           style={styles.textInput}
         />
 
         <View style={styles.pickerInput}>
           <Picker
             selectedValue={selectedTargetValue}
-            onValueChange={(itemValue, itemIndex) => { setSelectedTargetValue(itemValue); calculateConversion() }}>
+            onValueChange={(itemValue, itemIndex) => { setSelectedTargetValue(itemValue); calculateFromValue() }}>
             {coins.map(coin => <Picker.Item label={coin.name} value={coin.code} />)}
           </Picker>
         </View>
